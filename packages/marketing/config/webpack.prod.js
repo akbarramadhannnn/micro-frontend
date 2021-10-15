7207225488560d3f45b0bf564a5666ad13ce6552
akbarramadhannnn <akbarramadhan452@gmail.com>
2021-10-15 12:28:30 +0700

    added marketing deployment

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -7,6 +7,7 @@ module.exports = merge(commonConfig, {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
+    publicPath: '/marketing/latest/'
   },
   plugins: [
     new ModuleFederationPlugin({
