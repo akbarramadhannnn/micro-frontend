16fd6c0d8183c2d05c2ca5a17dee4c69472d6b29
akbarramadhannnn <akbarramadhan452@gmail.com>
2021-10-15 13:03:03 +0700

    fix css issue

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,27 +1,37 @@
 import React, { lazy, Suspense, useState } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 import Header from './components/Header';
 import Progress from './components/Progress';
 const MarketingApp = lazy(() => import('./components/MarketingApp'));
 const AuthApp = lazy(() => import('./components/AuthApp'));
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 const App = () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
   return (
     <BrowserRouter>
-      <div>
-        <Header
-          onSignOut={() => setIsSignedIn(false)}
-          isSignedIn={isSignedIn}
-        />
-        <Suspense fallback={<Progress />}>
-          <Route path="/" component={MarketingApp} />
-          <Route path="/auth">
-            <AuthApp onSignIn={() => setIsSignedIn(true)} />
-          </Route>
-        </Suspense>
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header
+            onSignOut={() => setIsSignedIn(false)}
+            isSignedIn={isSignedIn}
+          />
+          <Suspense fallback={<Progress />}>
+            <Route path="/" component={MarketingApp} />
+            <Route path="/auth">
+              <AuthApp onSignIn={() => setIsSignedIn(true)} />
+            </Route>
+          </Suspense>
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
